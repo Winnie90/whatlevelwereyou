777be06a6b996e5bcf8df1f230cf2c556cb94f39
chris <c.winstanley90@gmail.com>
2015-04-10 16:48:05 +0100

    get milestones

--- Models/Milestone.class.php
+++ Models/Milestone.class.php
@@ -1,5 +1,13 @@
 <?php
 
 class Milestone {
+    protected $milestoneId;
 
+    public function __construct($milestoneId) {
+        if($this->isValidMilestone($milestoneId)){
+            $this->milestoneId = $milestoneId;
+        } else {
+            throw new Exception("Is not valid milestone");
+        }
+    }
 } 
