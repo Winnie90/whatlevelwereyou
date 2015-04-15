46a0478149b7f35db9b21363e21ad88e1de0ab1b
chris <c.winstanley90@gmail.com>
2015-04-15 11:30:40 +0100

    Added test case for mode added mean

--- api/AbstractAPI.class.php
+++ api/AbstractAPI.class.php
@@ -34,7 +34,7 @@ abstract class API {
      * Allow for CORS, assemble and pre-process the data
      */
     public function __construct($request) {
-        header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: *");
         header("Content-Type: application/json");
 
