f3b6aefbae9db8226854111e89a912c079d0da35
chris <c.winstanley90@gmail.com>
2015-04-16 10:48:07 +0100

    added statistics test

---
+++ Tests/Models/MilestoneTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: chris
+ * Date: 15/04/15
+ * Time: 10:22
+  */
+
+class MilestoneTest extends PHPUnit_Framework_TestCase {
+
+}
+ 
---
+++ Tests/helpers/statsTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: chris
+ * Date: 15/04/15
+ * Time: 16:47
+  */
+
+class statsTest extends PHPUnit_Framework_TestCase {
+
+}
+ 
